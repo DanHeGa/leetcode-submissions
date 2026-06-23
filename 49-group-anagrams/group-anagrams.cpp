518dc9bf48303f7462217b902f6711e0e321f696
Daniela Herrera García <A00838702@tec.mx>
2026-06-22 23:06:42 -0600

    Time: 32 ms (19.50%) | Memory: 36.1 MB (6.37%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
+        map<vector<int>, vector<string>> mp;
         vector<vector<string>> res;
 
         for (string word : strs) {
-            string orgWord = word;
-            sort(word.begin(), word.end());
-            mp[word].push_back(orgWord);
+            vector<int> freqC(26, 0);
+            for (char letter : word) {
+                freqC[letter - 'a']++;
+            }
+
+            mp[freqC].push_back(word);
         }
 
         for (auto [head, anagrams] : mp) {
