f1fb28691c9cb82233b1130d0a88d607439fbd31
Daniela Herrera García <A00838702@tec.mx>
2026-07-05 17:34:57 -0600

    Time: 7 ms (38.64%) | Memory: 23.8 MB (82.44%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res = {intervals[0]};
+
+        int n = intervals.size();
+        for (int i = 1; i < n; i++) {
+            if (res.back()[1] >= intervals[i][0]) {
+                res.back()[0] = min(res.back()[0], intervals[i][0]);
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+            } else {
+                res.push_back(intervals[i]);
+            }
+        }
+
+        return res;
+    }
+};
