7b4defa37d9430eee2c8865d87896d137e924084
Daniela Herrera García <A00838702@tec.mx>
2026-07-06 18:53:41 -0600

    Time: 0 ms (100.00%) | Memory: 71.6 MB (98.66%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0];
+        int res = nums[0];
+
+        int n = nums.size();
+        for (int i = 1; i < n; i++) {
+            maxSum = max(nums[i], maxSum + nums[i]);
+            res = max(res, maxSum);
+        }
+
+        return res;
+    }
+};
