089725969cd3b349733326990c2dc4a442223588
Daniela Herrera García <A00838702@tec.mx>
2026-07-07 12:56:26 -0600

    Time: 1 ms (67.95%) | Memory: 16.8 MB (28.73%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/meeting-rooms-ii.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        int maxEndTime = 0;
+        //save meetings start and end times in order, like if we were using the maxEndtime length vector, but only analyzing the times when there is ac actual event/meeting staring/ending.
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> schedule;
         for (auto &inter : intervals) {
-            maxEndTime = max(maxEndTime, inter[1]);
-        }
-
-        vector<int> schedule(maxEndTime + 1, 0);
-        for (auto &inter : intervals) {
-            schedule[inter[0]]++;
-            schedule[inter[1]]--;
+            schedule.push({inter[0], 1});
+            schedule.push({inter[1], -1});
         }
 
         int maxSum = 0;
         int prefixSum = 0;
-        for(int i = 0; i < schedule.size(); i++) {
-            prefixSum += schedule[i];
+        while(!schedule.empty()) {
+            pair<int, int> currEvent = schedule.top();
+            schedule.pop(); 
+            prefixSum += currEvent.second;
             maxSum = max(maxSum, prefixSum);
         }
 
