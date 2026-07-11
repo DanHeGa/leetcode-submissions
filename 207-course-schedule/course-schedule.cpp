d4aa222947f45a81bb173717562545aee8614dfe
Daniela Herrera García <A00838702@tec.mx>
2026-07-11 10:43:34 -0600

    Time: 0 ms (100.00%) | Memory: 19.9 MB (38.55%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        //build adjList
+        unordered_map<int, vector<int>> adjList(numCourses);
+        vector<int> indegree(numCourses);
+        
+        for (auto &prereq : prerequisites) {
+            int course = prereq[0];
+            int pre = prereq[1];
+
+            indegree[course]++;
+            adjList[pre].push_back(course);
+        }
+
+        queue<int> q;
+        for (int i = 0; i < indegree.size(); i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        int courseCount = 0;
+        while(!q.empty()) {
+            int currCourse = q.front();
+            q.pop();
+            courseCount++;
+
+            for (int &dependant : adjList[currCourse]) {
+                indegree[dependant]--;
+                if (indegree[dependant] == 0) {
+                    q.push(dependant);
+                }
+            }
+        }
+
+        cout << numCourses << " and yours gave: " << courseCount << endl;
+        return courseCount == numCourses;
+    }
+};
