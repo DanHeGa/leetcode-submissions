cbf5ff8b1ef0b1c8f371ef3e01727d8cefe9cd3d
Daniela Herrera García <A00838702@tec.mx>
2026-07-30 12:57:10 -0600

    Time: 0 ms (100.00%) | Memory: 8.5 MB (71.21%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
+    double customPow(double x, int n) { //x^n
+        if (n == 0) return 1; //all num to the power of 1 is zero.
+
+        double half = customPow(x, n / 2);
+
+        if (n % 2 == 0) { //even exponent
+            return half * half;
+        } else { // odd
+            return x * (half * half);
+        }
+    }
+
     double myPow(double x, int n) {
-        return pow(x, n);
+        if( n < 0 ) {
+            double res = customPow(x, n);
+            return 1 / res;
+        } else {
+            return customPow(x, n);
+        }
     }
 };
