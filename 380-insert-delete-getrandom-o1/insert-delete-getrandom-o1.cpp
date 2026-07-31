ec5418c2a5a082f1648034b7e3febfc06ed980f3
Daniela Herrera García <A00838702@tec.mx>
2026-07-30 21:05:02 -0600

    Time: 49 ms (44.00%) | Memory: 113.1 MB (91.51%) - LeetSync

---
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -0,0 +1,57 @@
+class RandomizedSet {
+public:
+    vector<int> vecSet;
+    unordered_map<int, int> idxMap;
+
+    RandomizedSet() {
+        vector<int> vecSet;
+        unordered_map<int, int> idxMap;
+    }
+    
+    bool insert(int val) {
+        if (idxMap.contains(val)) {
+            return false; //we cannot insert an already existing value
+        }
+
+        vecSet.push_back(val);
+        idxMap[val] = vecSet.size() - 1;      
+        return true;
+    }
+    
+    bool remove(int val) {
+        if (idxMap.contains(val)) {
+            //switch val to remove with last one from the vecSet
+            int removeIdx = idxMap[val];
+            int lastIdx = vecSet.size() - 1;
+
+            int removeVal = vecSet[removeIdx];
+            int lastVal = vecSet[lastIdx];
+            vecSet[removeIdx] = lastVal;
+            vecSet[lastIdx] = removeVal;
+            
+            //update the map
+            idxMap[lastVal] = removeIdx;
+
+            //remove element from vector and map
+            vecSet.pop_back();
+            idxMap.erase(val);
+
+            return true;
+        }
+
+        return false; //non existent element
+    }
+    
+    int getRandom() {
+        int randomIdx = rand() % vecSet.size();
+        return vecSet[randomIdx];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
