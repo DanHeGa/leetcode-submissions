641b5fb50c2fb795c0d5ad11e9618184ef3b57f4
Daniela Herrera García <A00838702@tec.mx>
2026-08-01 14:09:05 -0600

    Time: 2 ms (34.29%) | Memory: 15.6 MB (77.78%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.cpp
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.cpp
@@ -11,46 +11,53 @@
  */
 class Solution {
 public:
-    map<int, vector<pair<int, int>>> columns;
-
-    void verticalInOrder(TreeNode* root, int weight, int depth){
-        if (!root) {
-            return;
-        }
-
-        verticalInOrder(root->left, weight - 1, depth + 1);
-        columns[weight].push_back({depth, root->val});
-        verticalInOrder(root->right, weight + 1, depth + 1);
-    }
-
     vector<vector<int>> verticalOrder(TreeNode* root) {
-        verticalInOrder(root, 0, 0);
-        vector<vector<int>> res;
-
-        for (auto weightGroup : columns) {
-            vector<pair<int, int>> nodes = weightGroup.second;
-            //sort by depth (because we already have them sorted by column order by using a map insstead of a unordered map)
-            stable_sort(nodes.begin(), nodes.end(),[](const pair<int, int>& a, const pair<int, int>& b) {
-                return a.first < b.first;
-            });
-
-            vector<int> subRes;
-            for (pair<int, int> pairNode : nodes) {
-                subRes.push_back(pairNode.second);
-            }
-
-            res.push_back(subRes);
+        if (!root) {
+            return {};
         }
 
-        return res;
+        map<int, vector<int>> columns;
+
+        queue<pair<int, TreeNode*>> q;
+        q.push({0, root});
+
+        while(!q.empty()) {
+            auto [weight, node] = q.front();
+            q.pop();
+
+            columns[weight].push_back(node->val);
+
+            if (node->left) {
+                q.push({weight - 1, node->left});
+            } 
+            if (node->right) {
+                q.push({weight + 1, node->right});
+            }
+        }
+
+        vector<vector<int>> ans;
+        for (auto column : columns) {
+            ans.push_back(column.second);
+        }
+
+        return ans;
     }
 };
 
 /*
+DFS approach (O(nlogn) in time, O(n) in space)
 inorder traversal 
 use weight for each node (root 0, left -1, right +1)
 in each visit, we append the element in the hashsmap with it's respective weight
 we sort the hahmap based on the key (weight)
 iterate over each elemnt in te hamap to push each value (group of nodess with same weight) into the result vector
 return res
+
+BFS approach (O(n) in time, O(n) in space)
+start queue with pair {0, root}, 0 is the column
+while queue not empty
+get curr node into map based on weight (first elemnt in the nodePair)
+pop curr node from queue
+add pair {weight - 1, leftnode} to queue if left child not null
+add pair {wight + 1, rightNode} to queue if right node not null
 */
