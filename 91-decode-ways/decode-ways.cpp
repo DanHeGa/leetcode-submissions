0276a6f8d2936439964e8f18cb68117c8742b948
Daniela Herrera García <A00838702@tec.mx>
2026-08-06 16:27:04 -0600

    Time: 0 ms (100.00%) | Memory: 8.7 MB (64.51%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.length();
+        vector<int> dp(n + 1);
+
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            if (s[i - 1] != '0'){
+                dp[i] = dp[i - 1];
+            }
+
+            if (i > 1 && s[i - 2] != '0' && s.substr(i - 2, 2) <= "26"){
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+};
+
+/*
+"226"
+      2  2  6
+dp[1, 0, 0, 0]
+*/
