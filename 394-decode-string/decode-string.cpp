db77b8f6f407383b31eca5526974a5060d8a9291
Daniela Herrera García <A00838702@tec.mx>
2026-08-06 19:56:09 -0600

    Time: 0 ms (100.00%) | Memory: 9.6 MB (17.98%) - LeetSync

---
+++ 394-decode-string/decode-string.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> numStk;
+        stack<string> subStrStk;
+
+        string result = "";
+        string currStr = "";
+        int currNum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (isdigit(s[i])) {
+                currNum = currNum * 10 + (s[i] - '0');
+            } else if (s[i] == '[') {
+                numStk.push(currNum);
+                currNum = 0;
+                subStrStk.push(currStr);
+                currStr = "";
+            } else if  (s[i] == ']') {
+                int currNum = numStk.top();
+                numStk.pop();
+                string prevStr = subStrStk.top();
+                subStrStk.pop();
+
+                string inner = "";
+                for (int i = currNum; i > 0; i--) {
+                    inner += currStr;
+                }
+
+                currStr = prevStr + inner;
+
+            } else { //it's a letter/char
+                currStr += s[i];
+            }
+        }
+
+        if (currStr != "") {
+            result += currStr;
+        }
+
+        return result;
+    }
+};
