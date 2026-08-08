25833e4efb71d528f31ce43ebd3ac518f7dfd372
Daniela Herrera García <A00838702@tec.mx>
2026-08-08 00:54:34 -0600

    Time: 0 ms (100.00%) | Memory: 12.4 MB (10.16%) - LeetSync

---
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.cpp
@@ -0,0 +1,58 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+    vector<Node*> flat(Node* head) { //we will return a vector with (start, end) of the level we are in
+        //create aux var = head
+        Node* aux = head;
+        //set start = head as well;
+        //end = start for the moment
+        Node* start = head;
+        Node* end = head;
+        //iterate while aux->next != nullptr
+        while(aux->next != nullptr || aux->child != nullptr) {
+            //if aux has a child
+            if (aux->child != nullptr) {
+                //(nextStart, nextEnd) = flat(child) //we will set the child as the ghead of it's level
+                vector<Node*> nxtLevelLimits = flat(aux->child);
+                Node* nextStart = nxtLevelLimits[0];
+                Node* nextEnd = nxtLevelLimits[1];
+                //attach next level to current level
+                Node* currNext = aux->next;
+                aux->next = nextStart;
+                nextStart->prev = aux;
+                //set child to null
+                aux->child = nullptr;
+                //attach end of next level to curr level
+                if (currNext != nullptr) {
+                    nextEnd->next = currNext;
+                    currNext->prev = nextEnd;
+                }
+            }
+
+            if (aux->next != nullptr) {
+                aux = aux->next;
+            }
+        }
+
+        end = aux;
+        
+        vector<Node*> res = {start, end};
+        return {start, end};
+    }   
+
+    Node* flatten(Node* head) {
+        if (!head) return nullptr;
+        vector<Node*> res = flat(head);
+        return res[0];
+    }
+};
