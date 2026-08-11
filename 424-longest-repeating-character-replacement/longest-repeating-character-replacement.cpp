0bb8deccf64a3c9bf2079a46a822cc177a5fe9b1
Daniela Herrera García <A00838702@tec.mx>
2026-08-10 23:17:50 -0600

    Time: 4 ms (57.07%) | Memory: 10.7 MB (93.83%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         //have a frequency map
-        unordered_map<char, int> freq;
+        vector<int> freq(26, 0);
 
         int maxFreq = 0;
 
         int l = 0;
         for (int r = 0; r < s.length(); r++) {
-            freq[s[r]]++;
-            maxFreq = max(maxFreq, freq[s[r]]);
+            freq[s[r] - 'A']++;
+            maxFreq = max(maxFreq, freq[s[r] - 'A']);
 
             int windowSize = r - l + 1;
             if (windowSize - maxFreq > k) {
-                freq[s[l]]--;
+                freq[s[l] - 'A']--;
                 l++;
             }
         }
