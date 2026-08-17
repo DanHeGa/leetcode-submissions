d0eec5e9b89cfe0e4179c53f594d6fe3f1b2b74d
Daniela Herrera García <A00838702@tec.mx>
2026-08-16 20:21:41 -0600

    Time: 0 ms (100.00%) | Memory: 12.5 MB (53.18%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
-    vector<int> runningSum(vector<int>& nums) {
-        vector<int> res(nums.size());
-        res[0] = nums[0];
-        
+    vector<int> runningSum(vector<int>& nums) {        
         for (int i = 1; i < nums.size(); i++) {
-            res[i] = nums[i] + res[i - 1];
+            nums[i] = nums[i] + nums[i - 1];
         }
 
-        return res;
+        return nums;
     }
 };
