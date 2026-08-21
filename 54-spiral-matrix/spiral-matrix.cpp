98595b1cc5df04cac03d22f8c31a8e9245464f4a
Daniela Herrera García <A00838702@tec.mx>
2026-08-20 22:45:46 -0600

    Time: 0 ms (100.00%) | Memory: 9.4 MB (57.69%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -7,19 +7,18 @@ public:
         int number_of_elements = rows * cols;
 
         vector<int> pairWise = {0, 1, 0, -1, 0}; //starting by going to the right
-        vector<vector<int>> visited(rows, vector<int>(cols, false));
         vector<int> ans;
         int i = 0;
         int j = 0;
         int k = 0;
         while(count <= number_of_elements) {
             ans.push_back(matrix[i][j]);
-            visited[i][j] = true;
+            matrix[i][j] = INT_MAX;
 
             int newRow = i + pairWise[k];
             int newCol = j + pairWise[k + 1];
 
-            if (newRow < 0 || newRow >= rows || newCol < 0 || newCol >= cols || visited[newRow][newCol]) {
+            if (newRow < 0 || newRow >= rows || newCol < 0 || newCol >= cols || matrix[newRow][newCol] == INT_MAX) {
                 k = (k + 1) % 4;
             }
             
