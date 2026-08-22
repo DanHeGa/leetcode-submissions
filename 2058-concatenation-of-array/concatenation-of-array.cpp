c36a4877f9a6978d7252453dcf786a56af524710
Daniela Herrera García <A00838702@tec.mx>
2026-08-21 22:00:48 -0600

    Time: 0 ms (100.00%) | Memory: 16.8 MB (81.32%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        vector<int> ans = nums;
-        for (int i = 0; i < nums.size(); i++) {
-            ans.push_back(nums[i]);
+        int n = nums.size();
+        vector<int> ans(n * 2);
+
+        int k = 0;
+        for (int i = 0; i < n * 2; i++) {
+            ans[i] = nums[k];
+            k = (k + 1) % n;
         }
 
         return ans;
