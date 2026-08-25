2b15897f64322ba773c878ef4c64a33db2f221f0
Daniela Herrera García <A00838702@tec.mx>
2026-08-24 21:45:45 -0600

    Time: 0 ms (100.00%) | Memory: 40.2 MB (81.23%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> leftToRight(n, 1);
-        vector<int> rightToLeft(n, 1);
-        leftToRight[0] = nums[0];
-        rightToLeft[n - 1] = nums[n - 1];
+        int leftProd = 1;
+        int rightProd = 1;
+        vector<int> res(n);
 
-        for (int i = 1; i < n; i++) {
-            leftToRight[i] = leftToRight[i - 1] * nums[i];
+        for (int i = 0; i < n; i++) { //crucial to uodate AFTER setting res[i]
+            res[i] = leftProd;
+            leftProd *= nums[i];
         }
 
-        for (int i = n - 2; i >= 0; i--) {
-            rightToLeft[i] = rightToLeft[i + 1] * nums[i];
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= rightProd;
+            rightProd *= nums[i];
         }
 
-        nums[0] = rightToLeft[1];
-        nums[n - 1] = leftToRight[n - 2];
-        for (int i = 1; i < n - 1; i++) {
-            nums[i] = leftToRight[i - 1] * rightToLeft[i + 1];
-        }
-
-        return nums;
+        return res;
     }
 };
