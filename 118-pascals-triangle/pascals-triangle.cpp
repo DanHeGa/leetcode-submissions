228c7cd5c693db1661d0218dcf83bbdf406cd62f
Daniela Herrera García <A00838702@tec.mx>
2026-08-27 20:42:49 -0600

    Time: 0 ms (100.00%) | Memory: 9.7 MB (29.24%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> triangle;
+        vector<vector<int>> triangle(numRows);
 
         for (int i = 0; i < numRows; i++) {
             vector<int> subVec(i + 1, 1);
-            triangle.push_back(subVec);
+            triangle[i] = subVec;
             //[1,2,1], start = 1, max = 1
             if (i > 0) {
                 for (int j = 1; j < subVec.size() - 1; j++) {
