fab58f15aacac86d55bec0610153dc7e602c2130
Daniela Herrera García <A00838702@tec.mx>
2026-09-06 18:56:52 -0600

    Time: 0 ms (100.00%) | Memory: 8 MB (56.07%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) return false;
+        
+        string concat = s + s;
+
+        return concat.contains(goal);
+    }
+};
