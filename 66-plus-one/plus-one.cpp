1b9f47ae9027b76214e35881bdebc043d4713e38
Daniela Herrera García <A00838702@tec.mx>
2026-09-07 11:39:28 -0600

    Time: 0 ms (100.00%) | Memory: 11.4 MB (93.75%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        reverse(digits.begin(), digits.end());
+
+        int carry = 0;
+        for (int i = 0; i < digits.size(); i++) {
+            if (i == 0) {
+                int ele = digits[i];
+                digits[i] = (digits[i] + 1) % 10;
+                carry = (ele + 1) / 10;
+                continue;
+            }
+
+            int op = digits[i] + carry;
+            digits[i] = op % 10;
+            carry = op / 10;
+        }
+
+        if (carry > 0){
+            digits.push_back(carry);
+        }
+        cout << "carry: " << carry;
+
+        reverse(digits.begin(), digits.end());
+        return digits;
+    }
+};
