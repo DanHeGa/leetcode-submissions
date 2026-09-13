fe0886fd11d416cf5132308dfdebc23934c75c1b
Daniela Herrera García <A00838702@tec.mx>
2026-09-12 22:21:05 -0600

    Time: 0 ms (100.00%) | Memory: 14.6 MB (24.58%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> set1(nums1.begin(), nums1.end());
+        if (nums2.size() > nums1.size()) {
+            //nums1 will always be the larger
+            vector<int> tmp = nums1;
+            nums1 = nums2;
+            nums2 = tmp;
+        }
+        
         unordered_set<int> set2(nums2.begin(), nums2.end());
 
         vector<int> res;
-        for (int ele : set1) {
+        for (int ele : nums1) {
             if (set2.contains(ele)) {
                 res.push_back(ele);
+                set2.erase(ele);
             }
         }
 
