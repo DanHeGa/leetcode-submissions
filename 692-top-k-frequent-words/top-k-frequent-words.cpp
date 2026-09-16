c7a02a99f74a78598d43465b7e61f23488a00f4e
Daniela Herrera García <A00838702@tec.mx>
2026-09-16 12:26:12 -0600

    Time: 3 ms (64.54%) | Memory: 18.2 MB (48.86%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -1,13 +1,12 @@
 class Compare {
     public:
         bool operator()(pair<string, int> const p1, pair<string, int> const p2){
-            if (p1.second < p2.second){
-                return true;
-            } else if (p1.second == p2.second) {
-                return p1.first > p2.first;
+            //in a pq, C++ interprets that, if compare is true, p1 has less priority than p2, placing p2 before p1.
+            if (p1.second != p2.second) {
+                return p1.second > p2.second;
             } 
 
-            return false;
+            return p1.first < p2.first;
         }
 };
 
@@ -15,22 +14,25 @@ class Solution {
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
         //count word frequencies
-        unordered_map<string, int> freqs;
-        for (string word : words) {
+        unordered_map<string, int> freqs; //O(m) m for number of words (space)
+        for (string word : words) { //O(n) time
             freqs[word]++;
         }
 
         //custom comparator for maxHeap
-        priority_queue<pair<string, int>, vector<pair<string, int>>, Compare> pq;
-        for (auto [word, freq] : freqs) {
+        priority_queue<pair<string, int>, vector<pair<string, int>>, Compare> pq; //space O(m)
+        for (auto [word, freq] : freqs) { //O(m) time
             pq.push({word, freq});
+            if (pq.size() > k) {
+                pq.pop(); //remove less frequent elements
+            }
         }
 
-        vector<string> res;
-        for (int i = 0; i < k; i++) {
+        vector<string> res(k); //O(k) space
+        for (int i = k - 1; i >= 0; i--) { //O(k) time
             pair<string, int> currEle = pq.top();
             pq.pop();
-            res.push_back(currEle.first);
+            res[i] = currEle.first;
         }
 
         return res;
