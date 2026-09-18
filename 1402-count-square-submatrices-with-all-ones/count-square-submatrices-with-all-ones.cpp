e1c495d1977b0298541ef2aa9a35c1edfbf8c393
Daniela Herrera García <A00838702@tec.mx>
2026-09-17 18:40:58 -0600

    Time: 4 ms (76.93%) | Memory: 30.4 MB (50.08%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -11,13 +11,14 @@ public:
         int squareCount = 0; 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if ((i - 1 < 0 || j - 1 < 0) && matrix[i][j] == 1) {
-                    dp[i][j] = 1;
-                    squareCount += dp[i][j];
+
+                if (matrix[i][j] == 0) {
                     continue;
                 }
 
-                if (matrix[i][j] == 1) {
+                if ((i - 1 < 0 || j - 1 < 0) && matrix[i][j] == 1) {
+                    dp[i][j] = 1;
+                } else if (matrix[i][j] == 1) {
                     dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
                 }
                 
