22d1166bcedcc61a84acbc3f257c17ad7d45b384
Daniela Herrera García <A00838702@tec.mx>
2026-09-17 18:41:41 -0600

    Time: 11 ms (33.57%) | Memory: 30.2 MB (68.72%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -18,7 +18,7 @@ public:
 
                 if (i - 1 < 0 || j - 1 < 0) {
                     dp[i][j] = 1;
-                } else if (matrix[i][j] == 1) {
+                } else {
                     dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
                 }
                 
