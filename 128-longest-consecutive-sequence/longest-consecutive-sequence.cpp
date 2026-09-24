14b69a26dece20685a15d1ca8f3d1ade488b86ce
Daniela Herrera García <A00838702@tec.mx>
2026-09-23 20:30:03 -0600

    Time: 195 ms (5.03%) | Memory: 118.7 MB (5.22%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,55 +1,57 @@
-struct UnionFind {
-    unordered_map<int, int> parents;
-    unordered_map<int, int> sizes;
-    int maxSize = 1;
+class UnionFind {
+    public:
+        unordered_map<int, int> parents;
+        unordered_map<int, int> sizes;
+        int maxSize = 1;
 
-    UnionFind(vector<int> nums) {
-        for (int num : nums) {
-            parents[num] = num;
-            sizes[num] = 1;
-        }
-    }
-
-    int find(int node) {
-        if (parents[node] != node) {
-            parents[node] = find(parents[node]);
+        UnionFind(vector<int>& nums) {
+            for (int num : nums) {
+                parents[num] = num;
+                sizes[num] = 1;
+            }
         }
 
-        return parents[node];
-    }
+        int find(int node) {
+            if (parents[node] != node) { //not using path compression
+                parents[node] = find(parents[node]);
+            }
 
-    void customUnion(int node1, int node2){
-        int root1 = find(node1);
-        int root2 = find(node2);
-
-        if (root1 == root2) {
-            return;
+            return parents[node]; //or node, which would be the same
         }
 
-        if (sizes[root1] < sizes[root2]) {
-            parents[root1] = root2;
-            sizes[root2] += sizes[root1];
-            maxSize = max(maxSize, sizes[root2]);
-        } else {
-            parents[root2] = root1;
-            sizes[root1] += sizes[root2];
-            maxSize = max(maxSize, sizes[root1]);
+        void _union(int node1, int node2) {
+            int p1 = find(node1);
+            int p2 = find(node2);
+
+            if (p1 == p2) {
+                return;
+            }
+            if (sizes[p2] > sizes[p1]) {
+                parents[p1] = p2;
+                sizes[p2] += sizes[p1];
+                maxSize = max(maxSize, sizes[p2]);
+            } else {
+                parents[p2] = p1;
+                sizes[p1] += sizes[p2];
+                maxSize = max(maxSize, sizes[p1]);
+            }
         }
-    }
 };
+
+
 
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         if (nums.size() == 0) return 0;
-        UnionFind uf(nums);
+        UnionFind u(nums);
 
         for (int num : nums) {
-            if (uf.parents.contains(num + 1)) {
-                uf.customUnion(num, num + 1);
+            if (u.parents.contains(num + 1)) {
+                u._union(num, num + 1);
             }
         }
 
-        return uf.maxSize;
+        return u.maxSize;
     }
 };
